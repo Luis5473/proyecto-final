7879823b575fa719e4cc5c5841f419ebc3c22d2c
Luisito9206 <luissabatela547@gmail.com>
2024-02-20 16:08:36 +0900

    Add debugging code

--- server.js
+++ server.js
@@ -15,17 +15,12 @@ const PORT = process.env.PORT;
 const password = process.env.PASSWORD_MONGO;
 const dbUser = process.env.User_MONGO;
 
-
-
-
-
-const url =`mongodb+srv://${dbUser}:${password}@cluster0.p56p1dd.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${dbUser}:${password}@cluster0.p56p1dd.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/healt", (req, res) => res.sendStatus(200))
-
+app.use("/healt", (req, res) => res.sendStatus(200));
 
 app.use(
   express.static("public", {
@@ -34,22 +29,23 @@ app.use(
         res.setHeader("Content-Type", "application/javascript");
       }
     },
-  }),
+  })
 );
 
 app.use("/", routes);
 
 const connectMongo = async () => {
   try {
+    console.log("url", url);
     await mongoose.connect(url);
     app.listen(PORT, () => {
       console.log(
-        `Servidor escuchando en el puerto ${PORT} y la base de datos conectada`);
+        `Servidor escuchando en el puerto ${PORT} y la base de datos conectada`
+      );
     });
   } catch (error) {
     console.log(error);
   }
 };
-
 
 connectMongo();
